72128932391baf9e1af8b305aefa4c950f393b65
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-08-03 22:25:26 +0500

    n

---
+++ index.js
@@ -0,0 +1,8 @@
+var uzs=prompt("Som'da pul miqdorini kiriting!")
+
+var usd=uzs/10654;
+var usd_round=Math.round(usd)
+var yevro=uzs/12674;
+var yevro_round=Math.round(yevro)
+alert(`${uzs} so'm =  ${usd} || ${usd_round} dollarda va ${uzs} so'm =  ${yevro} || ${yevro_round} yevroda`);
+
