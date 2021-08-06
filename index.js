40e3d5d3c2db69d530c648d780e46af3c6278935
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-08-06 15:22:09 +0500

    t

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var uzs=prompt("Som'da pul miqdorini kiriting!")
 
-var usd=uzs/10654;
-var usd_round=Math.round(usd)
-var yevro=uzs/12674;
-var yevro_round=Math.round(yevro)
-alert(`${uzs} so'm =  ${usd} || ${usd_round} dollarda va ${uzs} so'm =  ${yevro} || ${yevro_round} yevroda`);
+var yetish=uzs-500*9433.34-250*9433.34-120*10354.03;
+if(yetish>0){
+alert("Oq yo’l, Alisher!")
+}else{
+alert("Alisher, ozgina sabr qilish kerak bo’lar ekan")
+}
+
 
